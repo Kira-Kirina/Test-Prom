73b122bc9c3e1934124419b7c5571cc534d6746a
kira kirina <kira_lkirina@mail.ru>
2023-03-11 03:55:33 +0200

    best film storage3

--- src/app/services/film-card.service.ts
+++ src/app/services/film-card.service.ts
@@ -1,5 +1,13 @@
 import { Injectable } from '@angular/core';
-import { Observable, of, BehaviorSubject, Subject, tap } from 'rxjs';
+import {
+  Observable,
+  of,
+  BehaviorSubject,
+  Subject,
+  tap,
+  map,
+  merge,
+} from 'rxjs';
 import { IFilmCard } from 'src/models/film-card.interface';
 import data from '../../data.json';
 import { GENRES_MOCK } from 'src/genres-mock';
@@ -12,11 +20,34 @@ export class FilmCardService {
   private bestFilmSubject = new Subject<IFilmCard | null>();
   bestFilmObservable = this.bestFilmSubject.asObservable();
 
-  constructor() {}
+  private filmsSubject = new BehaviorSubject<IFilmCard[]>(this.FILMS_MOCK);
+  filmsObservable = this.filmsSubject.asObservable();
 
+  constructor() {}
   getFilmData(): Observable<IFilmCard[]> {
-    return of(this.FILMS_MOCK);
+    return this.filmsObservable;
+    // return of(this.FILMS_MOCK).pipe(
+    //   map((filmCards) => {
+    //     let cardId: number | null = null;
+    //     this.bestFilmObservable.subscribe((data) => {
+    //       console.log(cardId);
+    //       console.log(data?.id);
+    //       if (data?.id) {
+    //         cardId = data?.id;
+    //       }
+    //     });
+    //     filmCards.map((item) => {
+    //       if (cardId !== item.id) {
+    //         item.isBest = false;
+    //       }
+    //     });
+    //     return filmCards;
+    //   })
+    // );
   }
+  // getFilmData(): Observable<IFilmCard[]> {
+  //   return of(this.FILMS_MOCK);
+  // }
   getGenres(genres = GENRES_MOCK, genre: number[], filmGenres: string[]) {
     genres.forEach((item) => {
       for (const [key, value] of Object.entries(item)) {
@@ -39,6 +70,13 @@ export class FilmCardService {
       const cardJSON = JSON.stringify(card);
       localStorage.setItem('card', cardJSON);
       card.isBest = true;
+      let films = this.FILMS_MOCK.map((item) => {
+        if (card.id !== item.id) {
+          item.isBest = false;
+        }
+        return item;
+      });
+      this.filmsSubject.next(films);
       this.bestFilmSubject.next(card);
     }
   }
